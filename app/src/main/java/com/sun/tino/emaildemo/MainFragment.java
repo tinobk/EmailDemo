2057a8aeb76f51061e49dd613725579234497939
Tino <tungpt.sun@gmail.com>
2019-05-14 13:16:42 +0700

    view pager tablayout

---
+++ app/src/main/java/com/sun/tino/emaildemo/MainFragment.java
@@ -0,0 +1,22 @@
+package com.sun.tino.emaildemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
---
+++ app/src/main/java/com/sun/tino/emaildemo/SpamFragment.java
@@ -0,0 +1,22 @@
+package com.sun.tino.emaildemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SpamFragment extends Fragment {
+
+    public SpamFragment() {
+        // Required empty public constructor  return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spam, container, false);
+    }
+}
